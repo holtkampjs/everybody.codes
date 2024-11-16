36ad06f5296d367e4acf9062eac9e9c12094b112
Joseph Holtkamp <holtkampjs@gmail.com>
2024-11-16 00:05:37 -0600

    Updated requirements to handle groups of 2 enemies

--- src/combat_creatures.py
+++ src/combat_creatures.py
@@ -1,14 +1,34 @@
 ANCIENT_ANT: str = "A"
 BADASS_BEETLE: str = "B"
 CREEPY_COCKROACH: str = "C"
+DIABOLICAL_DRAGONFLY: str = "D"
+
+ENEMY_POTIONS_REQUIRED: dict = {
+    ANCIENT_ANT: 0,
+    BADASS_BEETLE: 1,
+    CREEPY_COCKROACH: 3,
+    DIABOLICAL_DRAGONFLY: 5
+}
 
 
 def count_potions_needed(enemies: str) -> int:
-    return enemies.count(BADASS_BEETLE) + enemies.count(CREEPY_COCKROACH)*3
+    potion_count: int = 0
+    for index in range(0, len(enemies), 2):
+        battle = enemies[index:index+2]
+        battle = battle.replace("x", "")
+        match len(battle):
+            case 1:
+                potion_count += ENEMY_POTIONS_REQUIRED.get(battle[0], 0)
+            case 2:
+                potion_count += ENEMY_POTIONS_REQUIRED.get(battle[0], 0)
+                potion_count += ENEMY_POTIONS_REQUIRED.get(battle[1], 0)
+                potion_count += 2
+    return potion_count
 
 
 if __name__ == "__main__":
-    with open("data/everybody_codes_e2024_q01_p1.txt", "r") as f:
+    # TODO: Use argparser to read in file name
+    with open("data/everybody_codes_e2024_q01_p2.txt", "r") as f:
         enemies: str = f.readline()
 
     potions_needed: int = count_potions_needed(enemies)
--- test/test_healing_potion_count.py
+++ test/test_healing_potion_count.py
@@ -4,12 +4,14 @@ from src.combat_creatures import count_potions_needed
 
 test_cases: list[tuple[str, int, str]] = [
     ("", 0, "No enemies should require no potions"),
-    ("A", 0, "Ancient ants require no potions"),
-    ("B", 1, "1 Badass Beetle should require 1 potion"),
-    ("C", 3, "1 Creepy cockroach should require 3 potions"),
-    ("AC", 3, "Should return 3 potions needed"),
-    ("BC", 4, "Should return 4 potions needed"),
-    ("AABBCC", 8, "Should require 8 potions")
+    ("Ax", 0, "1 Ancient Ant requires no potions"),
+    ("Bx", 1, "1 Badass Beetle should require 1 potion"),
+    ("Cx", 3, "1 Creepy cockroach should require 3 potions"),
+    ("Dx", 5, "1 Diablolical Dragonfly should require 5 potions"),
+    ("AC", 5, "Should require 5 potions"),
+    ("BC", 6, "Should return 6 potions needed"),
+    ("AABBCC", 14, "Should require 14 potions"),
+    ("AxBCDDCAxD", 28, "Should require 28 potions")
 ]
 
 
